94063834487a65a921f3651472e49eb04da622ac
Villa <villahrnn@hotmail.com>
2022-11-23 01:38:43 -0300

    Primer controller TextField

--- src/main/java/com/example/eventsfx/HelloController.java
+++ src/main/java/com/example/eventsfx/HelloController.java
@@ -2,7 +2,12 @@ package com.example.eventsfx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-
+    @FXML
+    private TextField nameField;
+    public void onButtonClicked(){
+        System.out.println("Hello, " + nameField.getText());
+    }
 }
